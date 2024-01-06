f85a0968a6bb9d5b7e7143d4122040da084484bf
Rajasekharpera <rajupera945@gmail..com>
2024-01-06 23:26:46 +0530

    React assignment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MissionGrid from "./dashboard";
+import PieChart from "./piechart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/AG" element={<Login />} />
+
+        <Route exact path="/Dashboard" element={<MissionGrid />} />
+        <Route exact path="/piechart" element={<PieChart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,83 @@
+import React, { createContext, useContext, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import { ThemeContext } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+
+const Login = () => {
+  const [email, setEmail] = useState("raju@gmail.com");
+  const [password, setPassword] = useState("rajuexam");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (email === "raju@gmail.com" && password === "rajuexam") {
+        navigate(`/Dashboard`);
+        console.log("Dashboard");
+        setEmail("");
+        setPassword("");
+      }
+    } catch (error) {
+      console.error("Error logging in:", error.message);
+      setError("Error logging in. Please try again later.");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h2>Login</h2>
+
+      <form className="">
+        <div className="">
+          <TextField
+            type="text"
+            variant="standard"
+            placeholder=""
+            label="Email"
+            value={email}
+            sx={{ width: "30ch" }}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="">
+          <TextField
+            type="password"
+            variant="standard"
+            placeholder=""
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            sx={{ marginBottom: "30px", marginTop: "30px", width: "30ch" }}
+          />
+        </div>
+        <div className="buttons">
+          <Button onClick={handleLogin} type="submit" variant="contained">
+            Login
+          </Button>
+<<<<<<< HEAD
+          <div className="signup">
+            {" "}
+          
+          </div>
+=======
+>>>>>>> 07df1781270efd937a04040d2b263184ff618412
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
